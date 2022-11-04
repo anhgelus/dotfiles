08d7de79ed62f5dfafc43240e08e93f16f91f026
Anhgelus Morhtuuzh <anhgelus.morhtuuzh@gmx.com>
2022-11-04 13:33:26 +0100

    build(sh) add update and install script

---
+++ install.sh
@@ -0,0 +1,4 @@
+cp $HOME/dotfiles/polybar/config.ini $HOME/.config/polybar/config.ini
+cp $HOME/dotfiles/i3/config $HOME/.config/i3/config
+cp $HOME/dotfiles/picom/picom.conf $HOME/.config/picom/picom.conf
+
---
+++ update.sh
@@ -0,0 +1,4 @@
+cp $HOME/.config/polybar/config.ini $HOME/dotfiles/polybar/config.ini
+cp $HOME/.config/i3/config $HOME/dotfiles/i3/config
+cp $HOME/.config/picom/picom.conf $HOME/dotfiles/picom/picom.conf
+
