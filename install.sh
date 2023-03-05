42006a8042c47ec0c1fa35fd55b793f23359d173
Anhgelus Morhtuuzh <anhgelus.morhtuuzh@gmx.com>
2023-03-05 21:33:23 +0100

    ci(install) install yay and some basic packages

--- install.sh
+++ install.sh
@@ -2,3 +2,18 @@ cp $HOME/dotfiles/polybar/config.ini $HOME/.config/polybar/config.ini
 cp $HOME/dotfiles/i3/config $HOME/.config/i3/config
 cp $HOME/dotfiles/picom/picom.conf $HOME/.config/picom/picom.conf
 
+# Install yay
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd ..
+rm -fr yay/
+
+# Install basic apps
+yay -Sy snap amberol flatpak picom-jonaburg-git polybar rofi feh xbindkeys alacritty fish xclip
+
+# Install oh my fish and setup the fish's theme
+curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+
+omf install slacker && omf theme slacker
+
