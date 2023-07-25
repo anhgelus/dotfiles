9f004955033387eebf3f58aec2501fba5020f307
Anhgelus Morhtuuzh <anhgelus.morhtuuzh@gmx.com>
2023-07-25 22:53:21 +0200

    feat(rofi): customize with adi1090x/rofi

--- update.sh
+++ update.sh
@@ -3,6 +3,7 @@ dotfiles=$HOME/dotfiles
 # Config
 cp $HOME/.config/polybar/config.ini $dotfiles/config/polybar/config.ini
 cp $HOME/.config/i3/config $dotfiles/config/i3/config
+cp -r $HOME/.config/rofi $dotfiles/rofi
 cp $HOME/.config/picom/picom.conf $dotfiles/config/picom/picom.conf
 cp $HOME/.config/nvim/init.vim $dotfiles/config/nvim/init.vim
 cp /etc/lightdm/slick-greeter.conf $dotfiles/etc/lightdm/slick-greeter.conf
