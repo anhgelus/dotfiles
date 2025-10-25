f7b2d69e6ff913d163dae11f1be31e05390273d1
William Hergès <william@herges.fr>
2025-10-25 19:26:51 +0200

    feat(fish): cli utils

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -40,3 +40,21 @@ set --export XBPS_DISTDIR "$HOME/.void-packages"
 
 # gpg
 export GPG_TTY=$(tty)
+
+# other
+set -gx EDITOR "nvim"
+
+fzf_configure_bindings --directory=\cf --git_log=\cg --git_status=\cs
+
+# yazi
+function y
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if read -z cwd < "$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
+
+# alias
+alias ll "eza -lha --icons=auto --sort=name --group-directories-first"
