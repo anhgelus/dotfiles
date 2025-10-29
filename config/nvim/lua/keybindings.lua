daee35570cf0323f69e150815eb69ce997c96195
William Hergès <william@herges.fr>
2025-10-29 21:49:58 +0100

    feat(nvim): bindings for lsp common things

--- config/nvim/lua/keybindings.lua
+++ config/nvim/lua/keybindings.lua
@@ -3,3 +3,11 @@ vim.keymap.set('n', '<A-Left>', '<cmd>tabprevious<CR>')
 -- Neotree
 vim.keymap.set('n', '<A-&>', '<cmd>Neotree<CR>')
 vim.api.nvim_create_user_command('Gs', 'Neotree float git_status git_base=main', {})
+-- LSP
+vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+vim.keymap.set('n', '<F6>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
