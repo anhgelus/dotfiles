a785d70f458adb198709e1cfe29dd3199562a40e
William Hergès <william@herges.fr>
2026-02-14 14:55:19 +0100

    feat(package): pulsemeeter

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -1,17 +1,8 @@
 return {
     {
-		"neovim/nvim-lspconfig",
-		config = function()
-			-- local lspconfig = require("lspconfig")
-
-			-- lspconfig.pylsp.setup({})
-		end,
-	},
-    { "mason-org/mason.nvim", opts = {} },
-    {
         "mason-org/mason-lspconfig.nvim",
         dependencies = {
-            "mason-org/mason.nvim",
+            { "mason-org/mason.nvim", opts = {} },
             "neovim/nvim-lspconfig",
         },
         opts = {
